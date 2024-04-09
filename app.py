c6d0181059a44fa889223af2bc9e64d2265e4c28
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2024-04-09 14:58:34 -0400

    More shit

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from langchain.prompts import ChatPromptTemplate
+"""from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredXMLLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import Chroma
+import torch
 
-loader = PyPDFLoader("./testdir/2304.08485.pdf")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+loader = PyPDFLoader("./data/R50-User-Manual Revised.pdf")
 pages = loader.load_and_split()
+
+# loader = UnstructuredXMLLoader("example_data/factbook.xml")
+# xml = loader.load_and_split()
+
+# loader = CSVLoader("./example_data/mlb_teams_2012.csv")
+# csv = loader.load()
+
+# data.extend(pages)
+
 vectorstore = Chroma.from_documents(documents=pages, embedding=GPT4AllEmbeddings())
 
 def format_docs(docs):
@@ -27,7 +41,8 @@ async def on_chat_start():
             ("human", "{question}"),
         ]
     )
-    runnable = prompt | model | StrOutputParser()
+    runnable = {"context": retriever, "issues_and_opportunities": RunnablePassthrough(), "business_goals": RunnablePassthrough(), "description": RunnablePassthrough()}
+    prompt | model | StrOutputParser()
     cl.user_session.set("runnable", runnable)
 
 @cl.on_message
@@ -45,4 +60,111 @@ async def on_message(message: cl.Message):
     ):
         await msg.stream_token(chunk)
 
+    await msg.send() 
+"""
+    #######
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import CTransformers
+import chainlit as cl
+
+DB_FAISS_PATH = "vectorstore/db_faiss"
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(
+        template=custom_prompt_template, input_variables=["context", "question"]
+    )
+    return prompt
+
+
+# Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 2}),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
+    )
+    return qa_chain
+
+
+# Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model="TheBloke/Llama-2-7B-Chat-GGML",
+        model_type="llama",
+        max_new_tokens=512,
+        temperature=0.5,
+    )
+    return llm
+
+
+# QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+    )
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+    return qa
+
+
+# output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({"query": query})
+    return response
+
+
+# chainlit code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
     await msg.send()
+    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain")
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
+
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
+
+    await cl.Message(content=answer).send()
